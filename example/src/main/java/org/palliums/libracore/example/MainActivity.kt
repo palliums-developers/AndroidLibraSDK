e6d02393ef3fd37ec5bd6738a47a359b780a0c8c
QuincySx <772804430@qq.com>
2020-10-13 15:59:39 +0800

    init

---
+++ example/src/main/java/org/palliums/libracore/example/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.palliums.libracore.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.palliums.libracore.exapmle.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
